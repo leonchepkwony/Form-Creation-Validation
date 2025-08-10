f152cdbb75662149c38758e8130d68b4ce201e04
Leonard Chepkwony <leonchepkwony@gmail.com>
2025-08-10 15:18:01 +0300

    Fetching Data from an API and Displaying It

---
+++ fetch-data.js
@@ -0,0 +1,27 @@
+async function fetchUserData() {
+  const apiUrl = "https://jsonplaceholder.typicode.com/users";
+  const dataContainer = document.getElementById("api-data");
+
+  try {
+    const response = await fetch(apiUrl);
+    const users = await response.json();
+
+    dataContainer.innerHTML = "";
+
+    const userList = document.createElement("ul");
+
+    users.array.forEach((user) => {
+      const userItem = document.createElement("li");
+      userItem.textContent = user.name;
+
+      userList.appendChild(userItem);
+    });
+    dataContainer.appendChild(userList);
+  } catch (error) {
+    dataContainer.innerHTML = "";
+    dataContainer.textContent = "Failed to load user data.";
+    console.log(error);
+  }
+}
+
+document.addEventListener("DOMContentLoaded", fetchUserData);
