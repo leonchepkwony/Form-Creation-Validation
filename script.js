98a77f0238b264d367bce06d04d8c1ffc5e22e32
Leonard Chepkwony <leonchepkwony@gmail.com>
2025-08-10 12:20:21 +0300

    updated script.js

--- script.js
+++ script.js
@@ -17,7 +17,7 @@ document.addEventListener("DOMContentLoaded", () => {
     const messages = [];
 
     // username validation
-    if (username.lenght === 3) {
+    if (username.length === 3) {
       isValid = false;
       messages.push("Username must be more than 3 characters!");
     }
@@ -29,7 +29,7 @@ document.addEventListener("DOMContentLoaded", () => {
     }
 
     // password validation
-    if (password.lenght < 8) {
+    if (password.length < 8) {
       isValid = false;
       messages.push("Password must be at least 8 characters long.");
     }
